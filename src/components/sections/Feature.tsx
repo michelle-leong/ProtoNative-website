9b94b9e922f5f490ee3211541a9d24df0262b2c8
Michelle Leong <leong.michellew@gmail.com>
2023-04-11 19:00:34 -0700

    alternate feature gifs

--- src/components/sections/Feature.tsx
+++ src/components/sections/Feature.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
 
 type Props = {
+  index: number;
   title: string;
   description: string;
   img: string;
+  gif: boolean;
 };
 
-const Feature = ({ title, description, img }: Props): JSX.Element => {
+const Feature = ({
+  index,
+  title,
+  description,
+  img,
+  gif,
+}: Props): JSX.Element => {
+  const className = gif ? 'gif' : 'picture';
   return (
     <div className='feature'>
-      <div className='feature-text'>
-        <h2>{title}</h2>
-        <p>{description}</p>
-      </div>
-      <img src={img} />
+      {index % 2 === 0 ? (
+        <div className='feature-text'>
+          <h2>{title}</h2>
+          <p>{description}</p>
+        </div>
+      ) : null}
+      <img className={className} src={img} />
+      {index % 2 === 1 ? (
+        <div className='feature-text'>
+          <h2>{title}</h2>
+          <p>{description}</p>
+        </div>
+      ) : null}
     </div>
   );
 };
